4cd5d978c8d2291ef50fbe76a1fdfd0085b14241
Sarami <baoh_sato@yahoo.co.jp>
2021-05-08 14:50:33 +0900

    Create content1.tsx

---
+++ components/content1.tsx
@@ -0,0 +1,12 @@
+import AutoSlideShow from 'components/auto-slide-show';
+import HexagonContent from 'components/hexagon-content'
+const Content1 = () => {
+  return(
+    <div>
+      <AutoSlideShow></AutoSlideShow>
+      <HexagonContent></HexagonContent>
+    </div>
+  )
+}
+
+export default Content1;
