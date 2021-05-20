ff0dedb6f87c3242a3dd1676df6168e6d0a81f13
Sarami <baoh_sato@yahoo.co.jp>
2021-05-20 19:05:22 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { FC } from 'react';
 import AutoSlideehow from '~/components/AutoSlideShow';
 import Main from '~/components/Main/index'
+import Speaker from '~/components/speaker';
 
 const Entrance: FC = () => <div>
-  <AutoSlideehow />
-  <Main />
+  {/* <AutoSlideehow />
+  <Main /> */}
+  <Speaker></Speaker>
 </div>;
 
 export default Entrance;
